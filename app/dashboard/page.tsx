e27ab7667cc379cc9bd73743c413eb1d8f3843f8
Jojoperandus-del <jojoperandus@gmail.com>
2026-04-25 12:13:41 +0200

    Delete app/dashboard directory

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-return (
-<main style={{ padding: "40px", fontFamily: "Arial" }}>
-<h1>Bienvenue sur ton dashboard</h1>
-<p>Tu es bien connecté 🎉</p>
-</main>
-);
-}
